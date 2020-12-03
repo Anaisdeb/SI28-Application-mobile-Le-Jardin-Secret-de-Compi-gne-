81e5596fd379dfec46f97312a1771c5ccf223aa8
Anais Debureaux <anais.debureaux@etu.utc>
2020-12-03 14:31:05 +0100

    Premier visuel de l'écran 1 avec icons

--- App1.tsx
+++ App1.tsx
@@ -1,28 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, Button, Alert } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as Location from 'expo-location';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null); //crée une fonction setErrorMsg qui prend la variable errorMsg
-
+  
+  
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
       setHasPermission(status === 'granted');
 
-      let { statusLoc } = await Location.requestPermissionsAsync();
+      
+	  let { statusLoc } = await Location.requestPermissionsAsync();
       if (statusLoc !== 'granted') {
         setErrorMsg('Pas de permission pour la géolocalisation');
       }
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+	  
     })();
   }, []);
+  
+  
 
   if (hasPermission === null) {
     return <View />;
@@ -31,27 +38,24 @@ export default function App() {
     return <Text>No access to camera</Text>;
   }
 
+  
   let text = 'En attente de la localisation..';
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
     text = JSON.stringify(location);
   }
+  
 
+  _onPressContinuer=()=> {
+			  Alert.alert("Continuons !");
+		  }
+  
+
+  
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 2 }}>
       <Camera style={{ flex: 1 }} type={type}>
-      <View style={{
-             flex: 1,
-             alignItems: 'center',
-             justifyContent: 'center',
-             marginTop: 0,
-           }}>
-           <Text style={{ fontSize: 30, marginBottom: 20, color: 'white',  textAlign: 'center' }}> Découvre un lieu magique </Text>
-		   <Text style={{ fontSize: 18, marginBottom: 20, color: 'white',  textAlign: 'center' }}> Grâce à cette application, tu vas découvrir un des lieux magiques de Compiègne : le Jardin Secret des Remparts </Text>
-		   
-           <Text style={{ fontSize: 18, marginBottom: 20, color: 'white' }}> {text} </Text>
-       </View>
         <View
           style={{
             flex: 1,
@@ -59,7 +63,7 @@ export default function App() {
             flexDirection: 'row',
           }}>
 
-          <TouchableOpacity
+          <TouchableOpacity //En appuyant dessus, l'opacité de la vue enveloppée est réduite, ce qui l'assombrit.
             style={{
               flex: 0.5,
               alignSelf: 'flex-end',
@@ -76,6 +80,46 @@ export default function App() {
           </TouchableOpacity>
         </View>
       </Camera>
+	  
+	  <View style={{
+		  flex:1,
+		  justifyContent: 'center',
+		  aligntItems: 'center'
+	  }}>
+		  <View style={{
+             flex: 1,
+             alignItems: 'center',
+			 alignSelf: 'flex-end',
+             justifyContent: 'center',
+             marginTop: 0,
+           }}>
+		   <View style={{ flexDirection: 'row'
+		   
+		   }}>
+           <Icon name="ios-radio-button-on" size={20}/><Icon name="ios-radio-button-off" size={20}/><Icon name="ios-radio-button-off" size={20}/>
+		   </View>
+		   <Text style={{ fontSize: 18, marginBottom: 40, marginTop: 20, color: 'black',  textAlign: 'center', marginHorizontal: 50, fontWeight: "bold" }}> DÉCOUVRE UN LIEU MAGIQUE </Text>
+		   <Text style={{ fontSize: 16, marginBottom: 20, color: 'black',  textAlign: 'center', marginHorizontal: 50, }}> Grâce à cette application, tu vas découvrir un des lieux magiques de Compiègne : le <Text style={{ fontWeight: "bold",}}>Jardin Secret des Remparts </Text> </Text>
+          </View>
+		  
+		  <View style={{backgroundColor:'black',
+						padding: 20,
+						marginHorizontal: 50,
+						marginBottom: 20,
+						borderRadius: 9,
+						shadowradius: 20,
+						shadowOpacity: 0.5,
+
+						}}>
+						
+			  <Button onPress={this._onPressContinuer}
+			  title="Continuer"
+			  color='black'
+			  >
+			  </Button>
+			
+		  </View>
+	  </View>	  
     </View>
   );
 }
--- android/app/src/main/java/com/compiegneinteractif/MainApplication.java
+++ android/app/src/main/java/com/compiegneinteractif/MainApplication.java
@@ -23,6 +23,8 @@ import expo.modules.permissions.PermissionsPackage;
 import expo.modules.filesystem.FileSystemPackage;
 import expo.modules.updates.UpdatesController;
 
+import com.oblador.vectoricons.VectorIconsPackage;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
@@ -41,7 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      List<ReactPackage> packages = new PackageList(this).getPackages();
+      List<ReactPackage> packages = new PackageList(this).getPackages(), new VectorIconsPackage();
       packages.add(new ModuleRegistryAdapter(mModuleRegistryProvider));
       return packages;
     }
