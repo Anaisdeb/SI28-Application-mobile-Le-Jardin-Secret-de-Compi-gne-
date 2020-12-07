6f0f33e9e32574b8cb0668b962f914f124cb8571
Guillaume Brocart <guillaume.brocart@etu.utc.fr>
2020-12-07 23:46:03 +0100

    basic nav

---
+++ Test.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+
+export default function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Un écran incroyable</Text>
+    </View>
+  );
+}
+
